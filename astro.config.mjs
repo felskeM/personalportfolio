ee85be21a39880babfd91e86da83b4baeddf0e32
Matthew Felske <mmayorf@outlook.com>
2026-02-25 20:04:36 +0000

    adding necessary astrojs integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import react from '@astrojs/react';
+
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  site: 'https://matthewfelske.com',
+  integrations: [react(), mdx()]
 });
